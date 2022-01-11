ae2b119d4b1c5d3aaa03edf071ee24432a03d4d2
Manu Reddy <reddybmanohar@gmail.com>
2022-01-11 19:16:48 +0530

    Roboshop payment & frontend main

--- Terraform-mutable/main.tf
+++ Terraform-mutable/main.tf
@@ -1,13 +1,13 @@
 module "payment" {
-  source = "git::https://github.com/ReddyManu/Terraform-mutable.git//app-module"
-  INSTANCE_TYPE = var.INSTANCE_TYPE
-  ENV = var.ENV
-  OD_INSTANCE_COUNT = var.OD_INSTANCE_COUNT
+  source              = "git::https://github.com/ReddyManu/Terraform-mutable.git//app-module"
+  INSTANCE_TYPE       = var.INSTANCE_TYPE
+  ENV                 = var.ENV
+  OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
   SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
-  COMPONENT = "payment"
-  PORT = 8080
-  IS_PRIVATE_LB = true
-  LB_RULE_PRIORITY = 103
+  COMPONENT           = "payment"
+  PORT                = 8080
+  IS_PRIVATE_LB       = true
+  LB_RULE_PRIORITY    = 103
 }
 
 output "INSTANCE_IDS" {
